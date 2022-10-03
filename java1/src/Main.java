e46a7395b3877a2906f3c70ef3c168ff6c1ba799
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-03 18:09:42 +0530

    strings and array

---
+++ java1/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println( "this is shortcut!");
+        System.out.println("this is awesome IDE for java!"); // sout for print data (it is an shortcut)
+        System.out.println("this is awesome IDE for java!");
+        System.out.println("mja aa gya bc!");
+        // this is comment ok
+        // primiytive data type
+
+        long phone = 1256734768777778887L;
+        float dec = 3434.5454f;
+        char letter = 't';
+
+        System.out.println(letter);
+        System.out.println(dec);
+        System.out.println(phone);
+        // non-primitive data type
+        String  name = "ravi";
+        String  name2 = "dhawan";
+        String name3 = name+" "+name2;
+        System.out.println(name3);
+        System.out.println(name3.charAt(0));
+        System.out.println(name3.charAt(1));
+        System.out.println(name3.charAt(2));
+        System.out.println(name3.charAt(3));
+        System.out.println(name3.charAt(4));
+        System.out.println(name3.charAt(5));
+        System.out.println(name2.substring(2,6));
+
+    }
+}
---
+++ java2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[]   marks = new int[5]; 
+        marks[0] = 245653;
+        marks[1] = 2673;
+        marks[2] = 76548;
+        marks[3]= 23345;
+        marks[4] = 2332;
+        for (int i = 0;i<5;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
