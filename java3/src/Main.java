d3d4f8bac8efa873b1d6d78a45a0989fedc08910
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-03 21:47:02 +0530

    take input as string and float  in java

--- java3/src/Main.java
+++ java3/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("input dedo:");
-        int age =  sc.nextInt();
+        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
+        int age =  sc.nextInt(); // for integer value input
+        float ac = sc.nextFloat();  // for floating value input
+        System.out.println(name);
         System.out.println("age is:"+age);
+        System.out.println("ac is:"+ac);
+
+
     }
 }
