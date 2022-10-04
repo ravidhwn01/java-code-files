69318a3e91b1eea46f7fd32f46119366d9116194
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-04 16:53:27 +0530

    inputs

--- java3/src/Main.java
+++ java3/src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("input dedo:");
-        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
+
         int age =  sc.nextInt(); // for integer value input
-        float ac = sc.nextFloat();  // for floating value input
+       // float ac = sc.nextFloat();  // for floating value input
+        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
         System.out.println(name);
         System.out.println("age is:"+age);
-        System.out.println("ac is:"+ac);
+      //  System.out.println("ac is:"+ac);
 
 
     }
