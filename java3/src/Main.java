8f075b9d83139838e6ca45c6c72cb819e625d226
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-04 17:02:23 +0530

    conditional statements using switch case

--- java3/src/Main.java
+++ java3/src/Main.java
@@ -2,10 +2,10 @@ import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("input dedo:");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input dedo:");
 //
-//        int age =  sc.nextInt(); // for integer value input
+        int age =  sc.nextInt(); // for integer value input
 //       // float ac = sc.nextFloat();  // for floating value input
 //        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
 //        System.out.println(name);
@@ -21,7 +21,27 @@ public class Main {
 //        } else {
 //            System.out.println("you are eligible!");
 //        }
-        
+
+        // 2 using switch case
+        switch (age){
+            case 12:
+                System.out.println("you are monkey");
+                break;
+                case 34:
+                System.out.println("you are tiger");
+                break;
+                case 6:
+                System.out.println("you are mongoose");
+                break;
+                case 1:
+                System.out.println("you are bitch");
+                break;
+                case 7:
+                System.out.println("you are dog");
+                break;
+            default:
+                System.out.println("you are nothing");
+        }
 
     }
 }
