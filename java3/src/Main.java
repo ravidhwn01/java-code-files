10ff95d4f4e047e44161604331adbcaded148f3b
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-04 16:57:14 +0530

    conditional statements

--- java3/src/Main.java
+++ java3/src/Main.java
@@ -2,16 +2,26 @@ import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("input dedo:");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("input dedo:");
+//
+//        int age =  sc.nextInt(); // for integer value input
+//       // float ac = sc.nextFloat();  // for floating value input
+//        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
+//        System.out.println(name);
+//        System.out.println("age is:"+age);
+//      //  System.out.println("ac is:"+ac);
 
-        int age =  sc.nextInt(); // for integer value input
-       // float ac = sc.nextFloat();  // for floating value input
-        String name = sc.nextLine();  // for string as a input this only take single word /tokens leta hai : next()
-        System.out.println(name);
-        System.out.println("age is:"+age);
-      //  System.out.println("ac is:"+ac);
 
+        // conditional statement
+        // 1 if else
+//        int age = 5;
+//        if (age<18){
+//            System.out.println("you are not eligible!");
+//        } else {
+//            System.out.println("you are eligible!");
+//        }
+        
 
     }
 }
