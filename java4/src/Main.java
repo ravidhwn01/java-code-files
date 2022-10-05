a94146a36d7765b43b32bc3ed016ad8e4031463e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-05 08:49:46 +0530

    error handling

---
+++ java4/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Expection handling
+        // TRY-CATCH in expection handling
+        int arr[] = {3423,34,5,6};
+        try{
+            System.out.println(arr[3]);
+
+        }catch (Exception exception){
+            System.out.println("this is error");
+        }
+        System.out.println("my name is lazy!");
+    }
+}
