201b49a157cb3beb27fe6a692f0057397a8e6ddf
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-05 09:00:16 +0530

    try-catch in expection handling

--- java4/src/Main.java
+++ java4/src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // Expection handling
         // TRY-CATCH in expection handling
-        int arr[] = {3423,34,5,6};
+//        int arr[] = {3423,34,5,6};
+        int a=5;
+        int b=0;
         try{
-            System.out.println(arr[3]);
+//            System.out.println(arr[3]);
 
+            System.out.println(b/a);
         }catch (Exception exception){
             System.out.println("this is error");
         }
