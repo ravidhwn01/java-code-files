887f1fd4a2b607093df42ad53f96333d3d7de65d
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-07 16:37:17 +0530

    string method

---
+++ java5/src/Main.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // using string methods in java 🤩
+        System.out.println(" hello ravi from this side! 😘 ");
+        String professional = "ravi dhawan is  a software engineer";
+        System.out.println(professional);
+        System.out.println(professional.toUpperCase());
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+        // java  String method
+        
+    }
+}
