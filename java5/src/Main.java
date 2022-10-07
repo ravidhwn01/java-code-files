79a6f74dba34fa7bd6d4197d5fb623dbce49ea97
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-07 16:50:35 +0530

    string method: touppercase

--- java5/src/Main.java
+++ java5/src/Main.java
@@ -5,12 +5,14 @@ public class Main {
         // using string methods in java 🤩
         System.out.println(" hello ravi from this side! 😘 ");
         String professional = "ravi dhawan is  a software engineer";
-        System.out.println(professional);
-        System.out.println(professional.toUpperCase());
+        System.out.println(professional.trim());
+        System.out.println(professional.toUpperCase()); //original string  will not change
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         System.out.println(str);
         // java  String method
-        
+        String ghar = new String("this is my home babe!🤩🤩🤩");
+        System.out.println(ghar);
+
     }
 }
